d74e02cc070cc0f78697ea71279ac68bc079fba7
YanAnderson-bit <yan.anderson135@gmail.com>
2023-06-18 13:50:38 -0300

    Enhances Tour model

--- src/models/TourModel.js
+++ src/models/TourModel.js
@@ -4,12 +4,46 @@ const TourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'The name is required'],
+    trim: true,
   },
-  rating: Number,
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a max group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, ' A tour must have a difficulty'],
+    trim: true,
+  },
+  ratingsAverage: { type: Number, default: 0 },
+  ratingsQuantity: { type: Number, default: 0 },
+
   price: {
     type: Number,
     required: [true, 'The price is required'],
   },
+  discount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, ' A tour must have a cover image'],
+    trim: true,
+  },
+  images: [String],
+  createdAt: { type: Date, default: Date.now() },
+  startDates: [Date],
 });
 
 export default mongoose.model('Tour', TourSchema);
